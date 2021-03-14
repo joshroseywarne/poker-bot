79c911ba8176e0887cbe627dda25b49100598591
Tristan Prazeus <tprazeus0110@gmail.com>
2021-03-14 15:38:31 +1100

    my token

---
+++ classes.js
@@ -0,0 +1,24 @@
+class Player {
+    constructor(username) {
+        //gives player a username and sets money
+    }
+
+    createHand() {
+        // gives a player a hand
+        // decide on how to store
+    }
+
+    setMoney(){
+        //changes a players money
+    }
+
+
+}
+
+class deck {
+    constructor() { ... }
+
+    handsHelp(){
+
+    }
+}
