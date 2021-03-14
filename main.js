9d02c594f9d990796f8015a1d1da8145ae4e4da9
Josh Rosewarne <jros0020@student.monash.edu>
2021-03-14 14:56:18 +1100

    set up env and gitignore

--- main.js
+++ main.js
@@ -4,3 +4,18 @@
 //then once howver many players go !hand or something it pms everyone their hand
 //i reckon just start everyone with however much money and then once a round ends everyone has to go !cum or something to keep going
 
+// require the discord.js module
+const Discord = require('discord.js');
+const dotenv = require('dotenv');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// login to Discord with your app's token, run it with the 
+client.login(process.env.TOKEN);
