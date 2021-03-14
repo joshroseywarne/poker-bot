b92f2882c9138fcffb9f9614b9b56f4276520248
Josh Rosewarne <jros0020@student.monash.edu>
2021-03-14 14:41:53 +1100

    set up

---
+++ main.js
@@ -0,0 +1,6 @@
+//bot token ODIwNDk1MDI2MDUxNjEyNzAy.YE1_ig.RQ6MWiRLFEgTHRD4zFTCX_2xOgc
+
+//intiate with like !poker [number of players]
+//then once howver many players go !hand or something it pms everyone their hand
+//i reckon just start everyone with however much money and then once a round ends everyone has to go !cum or something to keep going
+
